104e83cb5ba6ab555dec2d43763c5562b4968c41
דויד בקר <david.bekker@ruppin365.net>
2022-05-30 23:23:17 +0300

    Add project files.

---
+++ HW-5-Binary_Tree/Main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"BST.h"
+#include"reePrintLibrary.h"
+void main()
+{
+
+}
---
+++ HW-5-Binary_Tree/reePrintLibrary.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"BST.h"
+
+#ifndef _TREE_PRINT_LIBRARY_HEADER_
+
+struct TreeNode;// forward declaration
+
+extern __declspec(dllimport) void print_ascii_tree(TreeNode* t);
+
+#endif // !_TREE_PRINT_LIBRARY_HEADER_
